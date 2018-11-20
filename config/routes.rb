461256a2ff919be7430a37533c2c41a146a24b6f
Vincent Degove <vincent.degove@muage.net>
2018-11-20 15:02:01 +0100

    add new and create methods to task controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "tasks", to: "tasks#index"
   get "task/:id", to: "tasks#show", as: :task
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
 end
