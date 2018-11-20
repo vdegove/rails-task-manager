9c9700286c8a66a20b9ef6e6a4e2921d226b1ec7
Vincent Degove <vincent.degove@muage.net>
2018-11-20 16:02:06 +0100

    add edit, update and destroy methods to task controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "tasks", to: "tasks#index"
-  get "task/:id", to: "tasks#show", as: :task
   get "tasks/new", to: "tasks#new"
   post "tasks", to: "tasks#create"
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy", as: :task_delete
 end
